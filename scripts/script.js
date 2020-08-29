251da90012b75fc61b34f4a04e1f38409d361504
RMottram <16639499@students.lincoln.ac.uk>
2020-08-29 22:31:05 +0100

    Added typed js, tweaked font sizes and weights for desktop and mobile sizes and added carousel and parallax effect

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,15 @@ window.onload = function()
 {
     console.log("All good in da hood!");
 }
+
+
+// TYPING ANIMATION
+var typed = new Typed('.type', {
+    strings: ["hello world!","I design",
+    "I develop","I create","I ^2000make things..."],
+    backSpeed: 60,
+    backDelay: 100,
+    typeSpeed: 60,
+    startDelay: 500,
+    loop: true
+});
