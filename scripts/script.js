0327492191c8817cff7ca4f630ffdf530b67a2bb
RMottram <16639499@students.lincoln.ac.uk>
2020-08-31 18:22:52 +0100

    removed typed.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,3 @@ window.onload = function()
 {
     console.log("All good in da hood!");
 }
-
-
-// TYPING ANIMATION
-var heroTitle = new Typed('.type', {
-    strings: ["let name = \"Ryan Mottram\""],
-    typeSpeed: 80,
-    startDelay: 500,
-    loop: false
-});
